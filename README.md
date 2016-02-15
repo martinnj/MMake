3a69a12ecef2d7b4875eea3c8214b7bd6fbdd580
Martin Jørgensen <martinnjorgensen@gmail.com>
2016-02-15 19:08:46 +0100

    Working with arguments.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# mtmake
+# MtMake
 MultiThreaded Make wrapper. The idea is to build a dependency graph and
 compile the nodes on different threads.
 
 # Requirements
  - Python 3
+ - python3-pip
+ - [virtualenv](https://pypi.python.org/pypi/virtualenv)
+ - [Argument](https://pypi.python.org/pypi/argument)
+
+# Developing
+ - ```$ virtualenv -p /usr/bin/python3.4 venv```
+ - ```$ source venv/bin/activate```
+ - ```$ pip3 install -r requirements.txt```
 
 # License
 mtmake is licensed under the MIT license, see the [LICENSE](LICENSE) file for
--- mtmake.py
+++ mtmake.py
@@ -27,12 +27,49 @@
 #rulename <colon> <space seperated dependency list>
 #<tab> <command>
 
-import sys
+import argument
 
 def main():
     """
     """
 
+    f = argument.Arguments()
+    #Requried arguments, first argument will be stored as "candy"
+    f.always("candy", help="Candy name")
+
+    #optional unnamed value
+    f.maybe("soda")
+
+    #optional value, set a default, can be changed by adding: --num=30, or -n=30
+    f.option("num",
+        25,
+        help="How many pieces?",
+        abbr="n"
+        )
+    #add a switch, a flag with no argument
+    f.switch("reverse",
+        help="Reverse ordering",
+        abbr="r"
+    )
+    f.switch("unwrap", help="unwrapcandy", abbr="u")
+
+    #Process data before saving it
+    f.process("candy", lambda x: x.upper())
+    #Parse num as integer
+    f.process("num", lambda x: int(x))
+    f.validate("num", lambda x: x > 10)
+
+
+
+    #get data
+    arguments, errors = f.parse()
+
+    if len(errors) > 0:
+        print(errors)
+        print
+        print(f)
+    else:
+        print(arguments)
 
 if __name__ == '__main__':
     main()
