907142c93cef2b658f51856a5a4c61dcb207ac93
Martin J <hello@martinnj.dk>
2016-03-02 21:07:24 +0100

    More work on tests.

---
+++ tests/test_depgraph_dependencygraph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from depgraph import Node, DependencyGraph
+
+def test_dependenygraph_constructor():
+    pass
+
+def test_dependenygraph_add_independent():
+    pass
+
+def test_dependenygraph_add_dependency():
+    pass
+
+def test_dependenygraph_return_dependencies():
+    pass
+
+def test_dependenygraph_return_dependencies_str():
+    pass
+
+def test_dependenygraph_return_graph():
+    pass
+
+def test_dependenygraph_convert_makefile():
+    pass
